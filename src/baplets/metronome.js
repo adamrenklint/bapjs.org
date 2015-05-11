3739405c6c32c00b5204ec0cfd2121c6ab7a31c4
adamrenklint <adam@renklint.com>
2015-05-11 23:47:14 +0200

    it's really starting to look like something :)

---
+++ src/baplets/metronome.js
@@ -0,0 +1,19 @@
+  var kit = bap.kit();
+  console.log('foo')
+//   var basic = bap.oscillator({
+//     attack: 0.001,
+//     release: 0.1,
+//     length: 0.08
+//   });
+//
+//   kit.slot(1).layer(basic.with({ frequency: 330 }));
+//   kit.slot(2).layer(basic.with({ frequency: 440 }));
+//
+//   // create the pattern and add notes using expressions
+//   var pattern = bap.pattern({ bars: 2, tempo: 140 });
+//   pattern.channel(1).add(
+//     ['*.1.01', 'A1'],
+//     ['*.2%1.01', 'A2']
+//   );
+//
+//   pattern.kit('A', kit).start();
